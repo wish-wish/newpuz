201d158ee6e9fc8880651b02216d241f303d1fee
12092020 <12092020@qq.com>
2023-05-15 20:40:52 +0800

    update bat

--- release.bat
+++ release.bat
@@ -1,2 +1,1 @@
-javascript-obfuscator kodo.js --compact true --self-defending true --output kodo2.js && uglifyjs kodo2.js --output kodo.min.kd.js -c -m && del kodo2.js
-pause
+javascript-obfuscator kodo.js --compact true --self-defending true --output kodo2.js && uglifyjs kodo2.js --output kodo.min.kd.js -c -m && del kodo2.js && echo -- && echo  ›…ÌÕÍ≥… && echo -- && pause
